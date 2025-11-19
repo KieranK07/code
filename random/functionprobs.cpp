2edc73da11ad3e7c185820224f221e4dabcc8450
Kieran Kelly <kieranjokelly@gmail.com>
2025-11-19 09:13:02 -0500

    fib

--- random/functionprobs.cpp
+++ random/functionprobs.cpp
@@ -1,35 +1,23 @@
 #include <iostream>
 using namespace std;
 
-bool divides(int a, int b) {
-    return b % a == 0;
-}
-
-int exp(int b, int e) {
-    //figure out way to do exponential
-    int final = b;
-    for(int i=1; i<e; i++){
-        final = final*b;
-    }
-    return final;
-}
-
-int fib(int n) {
-    int output;
-    if (n==0||n==1){
-        cout << endl;
+//nth fibonacci number using recursion
+int fibonacci(int n) {
+    if (n <= 0)
+        return 0;
+    else if (n == 1)
         return 1;
-    }
-    else if (n>1) {
-        int fib1 = fib(n-1);
-        int fib2 = fib(n-2);
-
-        output = fib1 + fib2;
-        cout << output << "  ";
-    }
-    return output;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main() {
-    fib(20);
+    int n;
+    cout << "gimmeie postive int ";
+    cin >> n;
+
+    int result = fibonacci(n);
+    cout << "Fibonacci(" << n << ") = " << result << endl;
+
+    return 0;
 }
